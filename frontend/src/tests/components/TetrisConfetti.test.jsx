78c242f48d650781b6d3e66ae6fa42d42a46222a
Stanislav Krivtsov <s.krivtsov@quant-cube.com>
2024-08-23 11:54:19 +0200

    Confetti test covers 80%

--- frontend/src/tests/components/TetrisConfetti.test.jsx
+++ frontend/src/tests/components/TetrisConfetti.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import TetrisConfetti from '../../components/UI/TetrisConfetti';
 
 describe('TetrisConfetti Component', () => {
@@ -16,5 +16,23 @@ describe('TetrisConfetti Component', () => {
 
     const confetti = screen.queryByTestId('confetti');
     expect(confetti).not.toBeInTheDocument();
+  });
+
+  test('handles window resize correctly', () => {
+    const { rerender } = render(<TetrisConfetti show={true} setShow={() => {}} />);
+
+    // Trigger the resize event
+    act(() => {
+      window.innerWidth = 800;
+      window.innerHeight = 600;
+      window.dispatchEvent(new Event('resize'));
+    });
+
+    // Rerender to update the component with new dimensions
+    rerender(<TetrisConfetti show={true} setShow={() => {}} />);
+
+    const confetti = screen.getByTestId('confetti');
+    expect(confetti).toHaveAttribute('width', `${window.innerWidth - 10}`);
+    expect(confetti).toHaveAttribute('height', `${window.innerWidth < 1040 ? window.innerHeight * 1.9 : window.innerHeight}`);
   });
 });
